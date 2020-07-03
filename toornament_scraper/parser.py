211a0fe544b9d711e80703f95fb2dd7ebb40a652
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-07-03 13:00:40 -0500

    package as library

---
+++ toornament_scraper/parser.py
@@ -0,0 +1,121 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+from toornament_scraper.match import Match
+
+pp = pprint.PrettyPrinter(indent=4)
+
+
+class Parser(object):
+    baseUrl = 'https://www.toornament.com'
+    
+    def GetGroupRounds(self, url):
+        roundPage = requests.get(self.baseUrl + url)
+        roundSoup = BeautifulSoup(roundPage.text, features='html.parser')
+        roundList = []
+        
+        roundGrid = roundSoup.findAll('div', {'class': 'grid-flex vertical spacing-large'})
+        for round in roundGrid:
+            for a in round.findAll('a', href=True):
+                if a.text:
+                    roundList.append(self.baseUrl + (a['href']))
+        
+        return roundList
+        
+    def CalculateWinner(self, match, matchProperties):
+        for divs in matchProperties:
+            teams = divs.findAll('div', {'class': 'opponent'})
+            for team in teams:
+                if team.find('div', class_='result win'):
+                    if team.find('div', {'class': 'name'}).text.strip() == match.team1:
+                        match.winner = 1
+                        match.team1score = 1
+                        match.team2score = 0
+                    else:
+                        match.winner = 2
+                        match.team1score = 0
+                        match.team2score = 1
+        
+        return match
+
+    def GetMatchData(self, url):
+        matchPage = requests.get(url)
+        matchSoup = BeautifulSoup(matchPage.text, features="html.parser")
+        
+        matchProperties = matchSoup.findAll('div', {'class': 'match format-info'})
+        
+        if matchProperties[0].findAll('div', {'class': 'value'})[1].text == 'completed':
+            if matchSoup.select(
+                            '#main-container > div.layout-section.header > div > div.layout-block.header.mobile-hide.tablet-hide > div > div > div.state > div > div.result-1.forfeit.text'):
+                match = Match( \
+                    date=matchProperties[0].find('datetime-view')['value'], \
+                    completed=True, \
+                    group=matchProperties[0].findAll('div', {'class': 'value'})[5].text.strip(), \
+                    tournamentRound=matchProperties[0].findAll('div', {'class': 'value'})[6].text.strip(), \
+                    team1=matchProperties[1].findAll('div', {'class': 'name'})[1].text.strip(), \
+                    team2=matchProperties[1].findAll('div', {'class': 'name'})[2].text.strip(), \
+                    team1score='FORFEIT', \
+                    team2score='FORFEIT', \
+                    winner='FORFEIT', \
+                    url=url \
+                    )  # designate these as MISSING because they should be populated if status == completed
+            
+            else:
+                match = Match( \
+                    date=matchProperties[0].find('datetime-view')['value'], \
+                    completed=True, \
+                    group=matchProperties[0].findAll('div', {'class': 'value'})[5].text.strip(), \
+                    tournamentRound=matchProperties[0].findAll('div', {'class': 'value'})[6].text.strip(), \
+                    team1=matchProperties[1].findAll('div', {'class': 'name'})[1].text.strip(), \
+                    team2=matchProperties[1].findAll('div', {'class': 'name'})[2].text.strip(), \
+                    team1score='MISSING', \
+                    team2score='MISSING', \
+                    winner='MISSING', \
+                    url=url \
+                    )  # designate these as MISSING because they should be populated if status == completed
+                
+                match = self.CalculateWinner(match, matchProperties[1])
+        
+        else:
+            match = Match( \
+                date=matchProperties[0].find('datetime-view')['value'], \
+                completed=False, \
+                group=matchProperties[0].findAll('div', {'class': 'value'})[4].text.strip(), \
+                tournamentRound=matchProperties[0].findAll('div', {'class': 'value'})[5].text.strip(), \
+                team1=matchProperties[1].findAll('div', {'class': 'name'})[1].text.strip(), \
+                team2=matchProperties[1].findAll('div', {'class': 'name'})[2].text.strip(), \
+                team1score='NULL', \
+                team2score='NULL', \
+                winner='NULL', \
+                url=url \
+                )  # these are NULL because a match yet to happen has no winner...
+        
+        return match
+
+    def run(self, url):
+        groupPage = requests.get(url)
+        groupSoup = BeautifulSoup(groupPage.text, features="html.parser")
+        
+        matchList = []
+        finalOutput = []
+        
+        for x in range(16):
+            groupBlock = groupSoup.select(
+                "#main-container > div.layout-section.content > section > div > div:nth-child(2) > div > div:nth-child({})".format(
+                    x + 1))
+            for tag in groupBlock:
+                matchList.extend(self.GetGroupRounds(tag.find('a', href=True)['href']))
+        
+        print('Total Matches to be scraped: ' + str(len(matchList)))
+        for match in matchList:
+            try:
+                self.GetMatchData(match).PrintMatch()
+            except:
+                print('ERROR WITH MATCH - Contact Chorbadji at Leaguepedia API Discord')
+
+
+if __name__ == '__main__':
+    Parser().run('https://www.toornament.com/en_GB/tournaments/3543821601845821440/stages/3603290113079263232/')
+# RTL for Arabic teams needed?
+# logging? number inserted (completed/forfeited/still to play)
+# need totally different script likely for knockout stage

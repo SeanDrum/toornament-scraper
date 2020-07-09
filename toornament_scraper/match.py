57e15d16d8f5cb6bcb2261cd34382fc692aa755a
Sean <seandrummy1@gmail.com>
2020-07-08 20:46:51 -0400

    rewrote the scraper

--- toornament_scraper/match.py
+++ toornament_scraper/match.py
@@ -1,24 +1,24 @@
 class Match(object):
-    def __init__(self,date, completed, group, tournamentRound, team1, team2, team1score, team2score, winner, url):
+    def __init__(self,date, completed, team1, team2, team1score, team2score, winner):
         self.date = date
         self.completed = completed
-        self.group = group
-        self.tournamentRound = tournamentRound
+        # self.group = group
+        # self.tournamentRound = tournamentRound
         self.team1 = team1
         self.team2 = team2
         self.team1score = team1score
         self.team2score = team2score
         self.winner = winner
-        self.url = url
+        # self.url = url
     
     def PrintMatch(self):
         print(self.date)
         print(self.completed)
-        print(self.group)
-        print(self.tournamentRound)
+        # print(self.group)
+        # print(self.tournamentRound)
         print(self.team1)
         print(self.team2)
         print(self.team1score)
         print(self.team2score)
         print(self.winner)
-        print(self.url)
+        # print(self.url)
---
+++ toornament_scraper/parser2.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+from match import Match
+
+class Parser2(object):
+    def CalculateWinner(self, html, match):
+        winner = html.findAll('div', {'class': 'name'})[0].text.strip()
+
+        if winner == match.team1:
+            match.team1score = 1
+            match.team2score = 0
+            match.winner = 1
+        else:
+            match.team1score = 0
+            match.team2score = 1
+            match.winner = 2
+        
+        return match
+
+
+
+    def run(self):
+        baseUrl = 'https://www.toornament.com/en_GB/tournaments/3543821601845821440/matches/schedule?page='
+        numberOfPages = 35
+        currentPage = 1
+
+        for page in range(1, 5):
+            getUrl = baseUrl + str(page)
+            
+            pageList = requests.get(getUrl)
+            pageSoup = BeautifulSoup(pageList.text, features='html.parser')
+            roundList = []
+            
+            matchListRaw = pageSoup.findAll('div', {'class': 'grid-flex vertical spaceless'})
+            matchListDiv = matchListRaw[0].findAll('div', {'class': 'size-content'})
+            matchListFinal = []
+
+            for match in matchListDiv:
+                try:
+                    m = Match(\
+                        date=matchListRaw[0].find('datetime-view')['value'],\
+                            completed=True,\
+                            team1=match.findAll('div', {'class': 'name'})[0].text.strip(),\
+                            team2=match.findAll('div', {'class': 'name'})[1].text.strip(),\
+                            team1score='TBD',\
+                            team2score='TBD',\
+                            winner='TBD')
+                    m = CalculateWinner(match.findAll('div', {'class': 'opponent win'})[0], m)
+                    matchListFinal.append(m)
+
+                except:#every other row is going to have something worthwhile because of align-stretch divider
+                    pass
+            
+            return matchListFinal
+        
+if __name__ == '__main__':
+    Parser2().run()
+
